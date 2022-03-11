a8ebf2f2e9ca6d8f5dda1782beee60391897d7bf
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-03-11 17:33:34 +0530

    import gettext_lazy as _ added

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import BaseUserManager
-
+from django.utils.translation import gettext_lazy as _
 
 class BaseAccountManager(BaseUserManager):
     def create_user(self, first_name: str, last_name: str, email, password, **extra_fields):
